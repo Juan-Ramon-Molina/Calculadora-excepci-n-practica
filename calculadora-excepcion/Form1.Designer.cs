d03fbcdcc932c3c4e4efba02510fbeb37c4fc415
Juan Ramon Molina <jr.molina@outlook.es>
2025-12-18 13:29:14 -0300

    Primera carga

---
+++ calculadora-excepcion/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace calculadora_excepcion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBN1 = new System.Windows.Forms.TextBox();
+            this.TBN2 = new System.Windows.Forms.TextBox();
+            this.LBLResultado = new System.Windows.Forms.Label();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 252);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resultado:";
+            // 
+            // TBN1
+            // 
+            this.TBN1.Location = new System.Drawing.Point(348, 74);
+            this.TBN1.Name = "TBN1";
+            this.TBN1.Size = new System.Drawing.Size(100, 27);
+            this.TBN1.TabIndex = 3;
+            // 
+            // TBN2
+            // 
+            this.TBN2.Location = new System.Drawing.Point(348, 135);
+            this.TBN2.Name = "TBN2";
+            this.TBN2.Size = new System.Drawing.Size(100, 27);
+            this.TBN2.TabIndex = 4;
+            // 
+            // LBLResultado
+            // 
+            this.LBLResultado.AutoSize = true;
+            this.LBLResultado.Location = new System.Drawing.Point(349, 252);
+            this.LBLResultado.Name = "LBLResultado";
+            this.LBLResultado.Size = new System.Drawing.Size(69, 19);
+            this.LBLResultado.TabIndex = 5;
+            this.LBLResultado.Text = "######";
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(258, 184);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(190, 53);
+            this.BtnCalcular.TabIndex = 6;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 534);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.LBLResultado);
+            this.Controls.Add(this.TBN2);
+            this.Controls.Add(this.TBN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBN1;
+        private System.Windows.Forms.TextBox TBN2;
+        private System.Windows.Forms.Label LBLResultado;
+        private System.Windows.Forms.Button BtnCalcular;
+    }
+}
+
---
+++ calculadora-excepcion/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora_excepcion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       //rulo
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            int A, B, Resultado;
+            try
+            {
+                A = int.Parse(TBN1.Text);
+                B = int.Parse(TBN2.Text);
+                Resultado = A / B;
+                LBLResultado.Text = Resultado.ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Ingrese numeros");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("No se puede dividir por cero");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. Comuniquese con sistemas");
+            }
+            finally 
+            {
+                LBLResultado.BackColor = Color.Red;
+            }
+        }
+    }
+}
